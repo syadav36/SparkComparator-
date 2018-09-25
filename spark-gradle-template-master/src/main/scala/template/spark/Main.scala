c410797a8c8ebc292dff184cb3a5c42402895189
syadav36 <shailesh000047@gmail.com>
2018-09-26 00:14:24 +0530

    fourth commit

--- spark-gradle-template-master/src/main/scala/template/spark/Main.scala
+++ spark-gradle-template-master/src/main/scala/template/spark/Main.scala
@@ -33,7 +33,6 @@ object Main extends InitSpark {
 
 
 
-
     val df=spark.read
       .format("org.apache.spark.csv")
       .option("header", true)
@@ -55,6 +54,8 @@ object Main extends InitSpark {
     df1.show
 
 
+     // df.write.csv("/Users/shailesh/Documents/abc1.csv")
+
     df1.registerTempTable("TEMP_TABL")
 
 var sql_input="SELECT COUNT(id) AS ID,sum(id) ,firstName FROM TEMP_TABL GROUP BY firstName order by 3"
@@ -69,9 +70,76 @@ dff.show()
     println(final_check)
 
 
+//CONNECTING TO SQL SEVER***************************************
 
-    var columnsdf1=Seq((Array(1,1),"")).toDF("id","firstName").columns
-    var  columnsdf2 =Seq((Array(1,1),"")).toDF("id1","firstName").columns
+    /*
+    val jdbcHostname = "localhost"
+    val jdbcPort = 1433
+    val jdbcDatabase = "test"
+   val jdbcUsername="sa"
+   val jdbcPassword="Shailesh1"
+    Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+
+
+    // Create the JDBC URL without passing in the user and password parameters.
+   // val jdbcUrl = "jdbc:microsoft:sqlserver://localhost:1433/test"
+    val jdbcUrl = s"jdbc:sqlserver://${jdbcHostname}:${jdbcPort};database=${jdbcDatabase}"
+    // Create a Properties() object to hold the parameters.
+    import java.util.Properties
+    val connectionProperties = new Properties()
+
+    connectionProperties.put("user", s"${jdbcUsername}")
+    connectionProperties.put("password", s"${jdbcPassword}")
+
+    val driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+    connectionProperties.setProperty("Driver", driverClass)
+
+    val employees_table = spark.read.jdbc(jdbcUrl, "emp", connectionProperties)
+
+
+    employees_table.printSchema()
+    employees_table.select("*").show()
+
+*/
+//********************************sql SEVER READ END
+
+    //**********************************MYSQL Connect**************
+
+    /*
+    Class.forName("com.mysql.jdbc.Driver") // Databricks Runtime 3.3 and below
+
+    val jdbcHostname = "localhost"
+    val jdbcPort = 3306
+    val jdbcDatabase = "test"
+    val jdbcUsername="root"
+    val jdbcPassword="root"
+    // Create the JDBC URL without passing in the user and password parameters.
+    val jdbcUrl = s"jdbc:mysql://${jdbcHostname}:${jdbcPort}/${jdbcDatabase}"
+
+    // Create a Properties() object to hold the parameters.
+    import java.util.Properties
+    val connectionProperties = new Properties()
+
+    connectionProperties.put("user", s"${jdbcUsername}")
+    connectionProperties.put("password", s"${jdbcPassword}")
+
+
+    val employees_table = spark.read.jdbc(jdbcUrl, "emp", connectionProperties)
+
+
+    employees_table.printSchema()
+    employees_table.select("*").show()
+
+
+    import java.sql.DriverManager
+    val connection = DriverManager.getConnection(jdbcUrl, jdbcUsername, jdbcPassword)
+    connection.isClosed()
+    */
+
+//***********************************************mysql read end
+
+    var columnsdf1=Seq((Array(1,1))).toDF("id").columns
+    var  columnsdf2 =Seq((Array(1,1))).toDF("id1").columns
 
     var joinExprs1 = columnsdf1
       .zip(columnsdf2)
@@ -86,7 +154,7 @@ dff.show()
 
     var cols = df.columns.filter(_ != "id1").toList
     var df_res = df.alias("l").join(df1.alias("r"),joinExprs1,"full_outer")
-      .select($"id" :: cols.map(mapDiffs): _*)
+      //.select($"id" :: cols.map(mapDiffs): _*)
 
     df_res.show()
 
@@ -123,19 +191,15 @@ dff.show()
 
         val df_res_match=df_res
           .filter($"$source_col" === $"$target_col" ).dropDuplicates()
+          .select($"id" :: $"id1" ::cols.map(mapDiffs): _*)
 
 
+    df_res_match.show
 
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
 
-
-
-    df_res_match.show()
-
-
-
-
-
-
+    //x.withColumn("age", to_json(struct($"age"))).show()
 
 
     close
